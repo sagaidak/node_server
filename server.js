f6621e29e9117ed502c97675f832d830ffecc280
sagaidak <sagaidak1337@gmail.com>
2018-07-23 10:57:44 +0300

    projects page add

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects page',
+    welcomeMessage: 'Welcome to projects page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     error: 'Error message for this guy'
